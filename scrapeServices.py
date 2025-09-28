90a4e96ef67a1e09f533248453c0f38b70f5dad0
Fernando Villatoro <villatorofernando0623@gmail.com>
2025-09-27 23:16:44 -0700

    uh this shoit doesnt work

--- scrapeServices.py
+++ scrapeServices.py
@@ -2,9 +2,9 @@ import requests, json
 
 # the "Water bottle filling station" layer.
 BASE_QUERY_URL = 'https://gis.m.asu.edu/server/rest/services/Campus/CampusServices06022023/MapServer/15/query'
+filename = "output.json"
 
-
-def fetch_water():
+def fetch_all_water():
     offset = 0
     page_size = 2000
 
@@ -19,9 +19,16 @@ def fetch_water():
         r = requests.get(BASE_QUERY_URL, params=params, timeout=30)
         data = r.json()
 
-        filename = "output.json"
+        
 
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=4)
 
-fetch_all_features()
+def filter_water():
+    fetch_all_water()
+
+    with open(filename, 'r', encoding='utf-8') as f:\
+        content = f.read()
+    
+
+filter_water()
