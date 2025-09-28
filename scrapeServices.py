cd7e65762be176496a818a56888d60b9a1697877
Sarah Betts <sadsoups311@gmail.com>
2025-09-28 04:29:52 -0700

    Api scraping

--- scrapeServices.py
+++ scrapeServices.py
@@ -1,39 +1,38 @@
 import requests, json
 
-# the "Water bottle filling station" layer.
-BASE_QUERY_URL = 'https://gis.m.asu.edu/server/rest/services/Campus/CampusServices06022023/MapServer/15/query'
-filename = "output.json"
 
-def fetch_all_water():
-    offset = 0
-    page_size = 2000
+
+def fetch_all_services(SERVICE_ID, OUTFILE):
+    BASE_URL = "https://gis.m.asu.edu/server/rest/services/Campus/CampusServices06022023/MapServer/"+ str(SERVICE_ID) +"/query"
 
     params = {
         "where": "1=1",
         "outFields": "*",
         "f": "pjson", 
-        "resultOffset": offset,
-        "resultRecordCount": page_size
+        "resultOffset": 0,
+        "resultRecordCount": 2000
     }
-    r = requests.get(BASE_QUERY_URL, params=params, timeout=30)
+
+    r = requests.get(BASE_URL, params=params, timeout=30)
     data = r.json()
 
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(OUTFILE, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4)
 
-    print(f"Fetched {len(data['features'])} records.")
 
-def filter_water():
-    fetch_all_water()
+# Gets Water Filling Stations
+fetch_all_services(15, "waterStations.json")
+with open("waterStations.json", 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    features = data['features']
+    for feature in features:
+        buildingName = feature['attributes']['BLDG_NAME']
+        if buildingName is None:
+            print(feature, "\n") 
 
-    with open(filename, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        features = data['features']
-        for feature in features:
-            buildingName = feature['attributes']['BLDG_NAME']
-            if buildingName is None:
-                print(feature, "\n") 
+# Gets Emergency Call Boxes
+fetch_all_services(4, "emergencyCallBoxes.json")
 
-    
+# Gets Police Stations
+fetch_all_services(11, "policeStations.json")
 
-filter_water()
